cde21029aa81255b3257ade87a59cc2d4b8fdedf
Josebautista10 <cooldj1020@gmail.com>
2022-10-04 02:12:15 -0600

    add react-icons and finish adding all info to page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import { ReactComponent as HuddleDesign } from './images/illustration-mockups.svg'
 import { ReactComponent as Logo } from './images/logo.svg'
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 function App() {
   return (
@@ -9,16 +10,35 @@ function App() {
         <Logo />
       </header>
       <div className='main-container'>
-        <HuddleDesign className='huddle-design'/>
+        <HuddleDesign className='huddle-design' />
         <div className='info-container'>
           <h1>
             Build The Community {'\n'}
             Your Fans Will Love
           </h1>
-          <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
+          <p>
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genuine discussion.
+          </p>
           <button>Register</button>
         </div>
       </div>
+      <footer className='footer'>
+        <div className='icons-container'>
+          <a href='https://facebook.com/' target='blank_'>
+            <FaFacebookF className='icon' />
+          </a>
+
+          <a href='https://twitter.com/' target='blank_'>
+            <FaTwitter className='icon' />
+          </a>
+
+          <a href='https://instagram.com/' target='blank_'>
+            <FaInstagram className='icon' />
+          </a>
+        </div>
+      </footer>
     </div>
   )
 }
