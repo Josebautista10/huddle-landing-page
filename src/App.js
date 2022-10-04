537d7beebea0047182eca4e8aecbd151100d4db6
Josebautista10 <cooldj1020@gmail.com>
2022-10-04 00:33:57 -0600

    move images into src folder and import huddle design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css'
+import { ReactComponent as HuddleDesign } from './images/illustration-mockups.svg'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <header></header>
+      <HuddleDesign />
+    </div>
+  )
 }
 
 export default App
