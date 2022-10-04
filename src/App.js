ac0cb807f075da959585429597e6b0b2bd7c709d
Josebautista10 <cooldj1020@gmail.com>
2022-10-04 01:45:23 -0600

    finish adding information

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css'
 import { ReactComponent as HuddleDesign } from './images/illustration-mockups.svg'
+import { ReactComponent as Logo } from './images/logo.svg'
 
 function App() {
   return (
     <div className='App'>
-      <header></header>
-      <HuddleDesign />
+      <header className='header'>
+        <Logo />
+      </header>
+      <div className='main-container'>
+        <HuddleDesign className='huddle-design'/>
+        <div className='info-container'>
+          <h1>
+            Build The Community {'\n'}
+            Your Fans Will Love
+          </h1>
+          <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
+          <button>Register</button>
+        </div>
+      </div>
     </div>
   )
 }
